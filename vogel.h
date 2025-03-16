4806ff820794af2884c2ab20e8b2d448b2db304d
NguyenXuanKhang <102240314@sv1.dut.udn.vn>
2025-03-17 03:44:15 +0700

    Update vogel.h

--- vogel.h
+++ vogel.h
@@ -1,57 +1,57 @@
 #define MAX 10000
-float findTwoLowestValueInRow(int m, int n, float c[MAX][MAX]) {
+float findTwoLowestValueInRow(int n, int m, float c[MAX][MAX]) {
     float a[MAX];
-    for (int i = 0; i < m; i++) {a[i] = c[n][i];}
+    for (int i = 0; i < n; i++) {a[i] = c[n][i];}
     int i,I = -1;
     float m1 = 1e9, m2 = 1e9;
-    for (i = 0; i < m; i++) {
+    for (i = 0; i < n; i++) {
         if (a[i] < m1) {m1 = a[i]; I = i;}
     } a[I] = 1e9;
-    for (i = 0; i < m; i++) {
+    for (i = 0; i < n; i++) {
         if (a[i] < m2) {m2 = a[i];}
     } return m2 - m1;
 }
-float findTwoLowestValueInCol(int m, int n, float c[MAX][MAX]) {
+float findTwoLowestValueInCol(int n, int m, float c[MAX][MAX]) {
     float a[MAX];
-    for (int j = 0; j < n; j++) {a[j] = c[j][m];}
+    for (int j = 0; j < m; j++) {a[j] = c[j][m];}
     int j,J = -1;
     float m1 = 1e9, m2 = 1e9;
-    for (j = 0; j < n; j++) {
+    for (j = 0; j < m; j++) {
         if (a[j] < m1) {m1 = a[j]; J = j;}
     } a[J] = 1e9;
-    for (j = 0; j < n; j++) {
+    for (j = 0; j < m; j++) {
         if (a[j] < m2) {m2 = a[j];}
     } return m2 - m1;
 }
 
-void vogelMethod(int m, int a[MAX], int n, int b[MAX], float c[MAX][MAX], int x[MAX][MAX], float *totalCost) {
+void vogelMethod(int n, int a[MAX], int m, int b[MAX], float c[MAX][MAX], int x[MAX][MAX], float *totalCost) {
     int rowDoneYet[MAX] = {0}, colDoneYet[MAX] = {0}, totalRow[MAX] = {0}, totalCol[MAX] = {0};
     while (1) {
         float penRow[MAX], penCol[MAX];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < m; i++) {
 			penRow[i] = rowDoneYet[i]? -1e9 :findTwoLowestValueInRow(m, i, c);
 		}
-        for (int j = 0; j < m; j++) {
-			penCol[j] = colDoneYet[j]? -1e9 :findTwoLowestValueInCol(j, n, c);
+        for (int j = 0; j < n; j++) {
+			penCol[j] = colDoneYet[j]? -1e9 :findTwoLowestValueInCol(j, m, c);
 		}
         
         int I = -1, J = -1;
         float maxPenRow = -1e9, maxPenCol = -1e9;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < m; i++) {
 			if (penRow[i] > maxPenRow) {maxPenRow = penRow[i]; I = i;}
 		}
-        for (int j = 0; j < m; j++) {
+        for (int j = 0; j < n; j++) {
 			if (penCol[j] > maxPenCol) {maxPenCol = penCol[j]; J = j;}
 		}
         
         if (maxPenRow > maxPenCol) {
             J = -1;
-            for (int j = 0; j < m; j++) {
+            for (int j = 0; j < n; j++) {
 				if (!colDoneYet[j] && (J == -1 || c[I][j] < c[I][J])) {J = j;}
 			} 
         } else {
             I = -1;
-            for (int i = 0; i < n; i++) {
+            for (int i = 0; i < m; i++) {
 				if (!rowDoneYet[i] && (I == -1 || c[i][J] < c[I][J])) {I = i;}
 			} 
         }
@@ -66,10 +66,10 @@ void vogelMethod(int m, int a[MAX], int n, int b[MAX], float c[MAX][MAX], int x[
         if (totalRow[I] == b[I]) {rowDoneYet[I] = 1;}
 		
         int congratulation = 1;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < m; i++) {
 			if (!rowDoneYet[i]) {congratulation = 0;}
 		}
-        for (int j = 0; j < m; j++) {
+        for (int j = 0; j < n; j++) {
 			if (!colDoneYet[j]) {congratulation = 0;}
 		}
 
