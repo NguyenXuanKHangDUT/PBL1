73a60ba022469b3b52ee041f920a7853cb564c78
NguyenXuanKhang <102240314@sv1.dut.udn.vn>
2025-03-17 02:27:35 +0700

    Update northWest.h

--- northWest.h
+++ northWest.h
@@ -1,7 +1,7 @@
 #define MAX 10000
-void northWestMethod(int m, int a[MAX], int n, int b[MAX], float c[MAX][MAX], int x[MAX][MAX], float *totalCost) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {x[i][j] = 0;}
+void northWestMethod(int n, int a[MAX], int m, int b[MAX], float c[MAX][MAX], int x[MAX][MAX], float *totalCost) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {x[i][j] = 0;}
     }
     int i = 0, j = 0;
     int totalrow[100] = {0}, totalcol[100] = {0}; 
@@ -13,5 +13,5 @@ void northWestMethod(int m, int a[MAX], int n, int b[MAX], float c[MAX][MAX], in
         totalcol[j] += summer;
         if (totalrow[i] == b[i]) {i++;}
         if (totalcol[j] == a[j]) {j++;}
-    } while (i < n && j < m);
+    } while (i < m && j < n);
 }
